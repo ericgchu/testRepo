f185cba91262b3912c4570808d63ea03650f85d7
Eric Chu <echu123@umd.edu>
2022-01-15 21:11:29 -0500

    final fix

--- TestSub.java
+++ TestSub.java
@@ -2,7 +2,7 @@ import junit.framework.*;
 
 public class TestSub extends TestCase {
   public void testSub(){
-    int sum = Calc.add(2,4);
+    int sum = Calc.sub(2,4);
     assertTrue(sum == -2);
   }
 }
